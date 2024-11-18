3d68dd8a69a08b3b2ce4d3b255e6aeeb03e91cdf
actbit <57023457+actbit@users.noreply.github.com>
2024-11-18 22:10:01 +0900

    [fix]不要なコード削除

--- GraphVisualizer/wwwroot/service-worker.js
+++ GraphVisualizer/wwwroot/service-worker.js
@@ -1,4 +1,4 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
-self.addEventListener('fetch', event => { if (event.request.url.endsWith('.dll')) { event.respondWith(fetch(event.request)); } else { event.respondWith(caches.match(event.request).then(response => { return response || fetch(event.request); })); } });
+self.addEventListener('fetch', () => { });
